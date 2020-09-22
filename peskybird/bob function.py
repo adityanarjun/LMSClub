6bc74e543bb5302d959b61faeabf23843157428d
Aditya <rathi.kartheek24@gmail.com>
2020-09-21 21:49:56 -0700

    function printing

---
+++ peskybird/bob function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#This is my bob function declaration
+def bob1():
+    print "hello I am bob"
+    
+#this is my bob function declaration
+def bob2(ver):
+    print "Hi bob how are you doing, "+ver+""
+
+
+bob1()
+bob2("minecraft")
